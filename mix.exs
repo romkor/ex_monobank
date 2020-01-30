cb265c9bd901909fb658c2d129f638666d99c125
Roman Rodych <romko.ua@gmail.com>
2020-01-30 12:51:40 +0200

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -31,7 +31,8 @@ defmodule ExMonobank.MixProject do
   defp deps do
     [
       {:tesla, "~> 1.3.0"},
-      {:jason, ">= 1.0.0"}
+      {:jason, ">= 1.0.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
