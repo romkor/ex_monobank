8188d63ec090baeb3a6741a4973d343e76b00df7
Roman Rodych <romko.ua@gmail.com>
2020-02-07 23:32:36 +0200

    Bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExMonobank.MixProject do
   def project do
     [
       app: :ex_monobank,
-      version: "2.0.2",
+      version: "2.0.3",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       package: package(),
